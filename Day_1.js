9377b1df08d93d260ca231cd9a2f373e1445eadf
Arham Begani <arhambegani2@gmail.com>
2025-04-24 10:13:54 +0530

    Day-1
    Day 1 of learning Javascript

---
+++ Day_1.js
@@ -0,0 +1,53 @@
+//                          Day 1
+// Learning Variables
+
+let first_name = "Arham";
+
+console.log(typeof first_name);
+console.log(first_name);
+
+//Entering a variable in a console.log()
+
+let age = 25;
+let name = 'Arham';
+
+console.log(`Your name is ${name}`);
+console.log(`You are ${age} years old`)
+
+//Boolean
+
+let online = true
+
+console.log(`You are online: ${online}`)
+console.log(typeof online)
+
+//getElementById
+
+let fullname = "Arham Begani";
+let myage = 14;
+let student = true;
+
+document.getElementById("p1").textContent = `Your name is ${fullname}`;
+document.getElementById("p2").textContent = `You are ${myage} years old`;
+document.getElementById("p3").textContent = `Enrolled: ${student}`;
+
+// Arithmetic operators
+
+let num = 30;
+
+//num = num % 2;
+//num = num + 1;
+//num = num - 1;
+//num = num / 3;
+
+//num += 1;
+//num -= 1;
+//num *= 2;
+//num /= 2;
+//num %= 2;
+//num **= 2;
+
+//num++;
+//num--;
+
+console.log(num)
